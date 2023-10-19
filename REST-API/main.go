eabff9e1fc5838d5b78cdb7a0f05c441cc618e79
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2023-10-20 02:16:16 +0800

    rest-api (graceful shutdown)

--- REST-API/main.go
+++ REST-API/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"restful/config"
+	"syscall"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -21,7 +25,29 @@ func main() {
 		WriteTimeout: time.Duration(config.App.WriteTimeout) * time.Second,
 	}
 
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalln("listen:", err)
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalln("listen:", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
 	}
+
+	log.Println("Server exiting")
 }
