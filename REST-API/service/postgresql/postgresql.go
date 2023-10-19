d663a531589ff34f1839cd5590fbf83785ed4614
SuperAppKid <34574973+SuperAPPKid@users.noreply.github.com>
2023-10-20 07:43:11 +0800

    rest-api: modify service package structure

---
+++ REST-API/service/postgresql/postgresql.go
@@ -0,0 +1,33 @@
+package postgresql
+
+import (
+	"fmt"
+	"restful/config"
+	"sync"
+
+	postgresDriver "gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	once sync.Once
+	c    conn
+)
+
+type conn struct {
+	*gorm.DB
+}
+
+func Start() conn {
+	// lazy init db connection
+	once.Do(func() {
+		conf := config.PostgreSQL
+		dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", conf.Host, conf.Port, conf.Username, conf.Password)
+		db, err := gorm.Open(postgresDriver.Open(dsn), &gorm.Config{})
+		if err != nil {
+			panic(err)
+		}
+		c = conn{db}
+	})
+	return c
+}
